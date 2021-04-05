b35f2194fc989a3e23c7fcb0461242c9474f055d
umetsu <umetsu.kentaro@adways.net>
2021-04-05 11:26:43 +0900

    :sparkles: Headerコンポーネント追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
+import { Header } from '../components/Header'
 
 export default function TopPage() {
   return (
-    <Container maxWidth={'md'} style={{ padding: '16px' }}>
-      <>Hello Next.js</>
-    </Container>
+    <>
+      <Header />
+      <Container maxWidth={'md'} style={{ padding: '16px' }}>
+        <>Hello Next.js</>
+      </Container>
+    </>
   )
 }
